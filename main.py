d662ee4b0f51c7d6f0847e524c77ce0c21870849
shanimergui <69587799+shanimergui@users.noreply.github.com>
2023-04-11 14:41:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+from scipy.optimize import linear_sum_assignment
+from scipy.spatial import distance_matrix
 from skimage.feature import blob_log
-from skimage.color import rgb2gray
+from skimage.color import rgb2gray, rgb2yuv
 import numpy as np
 import cv2
 
 
 def find_coordinates(img_path, output_path=None):
+    # Read the image using OpenCV
     image = cv2.imread(img_path)
-    gray = rgb2gray(image)
 
-    blobs_log = blob_log(gray, max_sigma=30, num_sigma=10, threshold=.1)
+    # Convert the image to YUV color space and use the Y channel only
+    yuv_image = rgb2yuv(image)
+    yuv_image = yuv_image[:, :, 0]
 
+    # Detect stars using blob detection with Laplacian of Gaussian (LoG) method
+    blobs_log = blob_log(yuv_image, max_sigma=30, num_sigma=10, threshold=.1)
+
+    # For each detected blob, calculate its brightness and store its coordinates
     coordinates = []
     for i, blob in enumerate(blobs_log):
         y, x, r = blob
-        brightness = np.mean(gray[int(y - r):int(y + r), int(x - r):int(x + r)])
+        brightness = np.mean(yuv_image[int(y - r):int(y + r), int(x - r):int(x + r)])
         coordinates.append((x, y, r, brightness))
+
+    # Write the coordinates to a file if output_path is provided
     if output_path is not None:
         with open(output_path, 'w') as f:
             for result in coordinates:
@@ -27,28 +37,34 @@ def distance(point1, point2):
 
 
 def match_stars(img1_path, img2_path, output_path=None):
-    stars1 = find_coordinates(img1_path)
-    stars2 = find_coordinates(img2_path)
+    """
+    Finding star matches between two images using the RANSAC algorithm.
+
+    :param img1_path: Path to the first image.
+    :param img2_path: Path to the second image.
+    :param output_path: (Optional) Path to write the output file containing the matching star pairs.
+    :return: A list of tuples, where each tuple contains the coordinates and radius of a matching star in both images.
+    """
+    stars1 = np.array(find_coordinates(img1_path))
+    stars2 = np.array(find_coordinates(img2_path))
+
+    # Create a distance matrix between all stars in both images
+    dist_matrix = distance_matrix(stars1[:, :2], stars2[:, :2])
+
+    # Convert the distance matrix to a cost matrix for the linear sum assignment problem
+    cost_matrix = (1 - dist_matrix / 50)
+
+    # Solve the linear sum assignment problem
+    row_ind, col_ind = linear_sum_assignment(-cost_matrix)
+
+    # Create a list of matching star pairs
     matching_pairs = []
-    max_matching_score = 1
-    for star1 in stars1:
-        best_matching_score = max_matching_score
-        matching_star = None
+    for row, col in zip(row_ind, col_ind):
+        # We will only add to the list the pairs whose matching cost is positive.
+        if cost_matrix[row, col] > 0:
+            matching_pairs.append((tuple(stars1[row]), tuple(stars2[col])))
 
-        for star2 in stars2:
-            dist = distance(star1, star2)
-
-            if dist < 50:
-                matching_score = 1 - (dist / 50)
-
-                if matching_score < best_matching_score:
-                    best_matching_score = matching_score
-                    matching_star = star2
-
-        if matching_star is not None:
-            matching_pairs.append((star1, matching_star))
-            stars2.remove(matching_star)
-
+    # Write the output file if a path is provided
     if output_path is not None:
         with open(output_path, "w") as file:
             for pair in matching_pairs:
